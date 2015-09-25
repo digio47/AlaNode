b00955bef481d5e1b5f5e16113ecd158d5ded59a
digio47 <digio47@gmail.com>
2015-09-25 16:51:25 +0200

    rimosso info personali

--- mynode.js
+++ mynode.js
@@ -25,12 +25,16 @@ mynode.controller('SiteConfigController',function($scope){
 });
 
 mynode.controller('MyController', function($scope){
+   $scope.datas = [
+      { "name":"Mario", "lastname":"Marrazzo", "age":45, "address":"Via Mare, 6" },
+      { "name":"Laura", "lastname":"Russo", "age":25, "address":"Via Roma, 12" },
+      { "name":"Francesco", "lastname":"Terzi", "age":31, "address":"Via Teora, 4" }
+   ]
+});
 
-   $scope.name = "Mario";
-   $scope.datas = {
-      "name":"Mario",
-      "lastname":"Marrazzo",
-      "age":45,
-      "address":"Via simone lala, 6"
-   }
+mynode.controller('UrlDatas', function($scope, $http){
+   $http.get("http://www.w3schools.com/angular/customers.php")
+       .success(function(response) {
+          $scope.records = response.records;
+       });
 });
